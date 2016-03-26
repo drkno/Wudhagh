2040f818c45ccb07cb3b90aa0252dc3ba8bdc1d8
Matthew <mrk45@uclive.ac.nz>
2016-03-27 00:53:51 +1300

    Migration to routes and addition of swipe indicator

---
+++ www/js/app.js
@@ -0,0 +1,32 @@
+﻿var Wudhagh = angular.module('Wudhagh', [
+    'ngRoute',
+    'ui.bootstrap',
+    'ngTouch',
+    'wudhaghControllers'
+]),
+
+routes = [
+    {
+        path: '/shoppinglist',
+        templateUrl: 'pages/shoppinglist.html',
+        controller: 'ShoppingListController',
+        name: 'Shopping List'
+    }
+],
+
+defaultRoute = 0;
+
+Wudhagh.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+
+    var def = $routeProvider;
+    for (var i = 0; i < routes.length; i++) {
+        def = def.when(routes[i].path, routes[i]);
+    }
+    def.otherwise({
+        redirectTo: routes[defaultRoute].path
+    });
+
+//    $locationProvider.html5Mode(true);
+}]);
+
+var wudhaghControllers = angular.module('wudhaghControllers', ['ui.bootstrap', 'ngTouch']);
